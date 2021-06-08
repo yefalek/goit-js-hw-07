dbf6fc703a64c134a5d8be8babbc7c4c0150aca7
yefalek <yefremenkov.alek@gmail.com>
2021-06-08 21:44:20 +0300

    fix

--- js/task-08.js
+++ js/task-08.js
@@ -1,29 +1,32 @@
-var render = document.querySelector('[data-action="render"]');
-var destroy = document.querySelector('[data-action="destroy"]');
-var boxes = document.getElementById('boxes');
+let render = document.querySelector('[data-action="render"]');
+let destroy = document.querySelector('[data-action="destroy"]');
+let boxes = document.getElementById('boxes');
+
 render.addEventListener('click', getAmount);
-destroy.addEventListener('click', destroyBoxes);
 
 function getAmount() {
-  var amount = +document.querySelector('#controls input').value;
+  const amount = +document.querySelector('#controls input').value;
   createBoxes(amount);
 }
+destroy.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
 
 function createBoxes(amount) {
-  var basicSize = 30;
-  var fragment = document.createDocumentFragment();
+  const basicSize = 30;
+  let fragment = document.createDocumentFragment();
   for (var i = 0; i < amount; i++) {
-    var size = basicSize + i * 10;
-    var div = document.createElement('div');
+    let size = basicSize + i * 10;
+    let div = document.createElement('div');
     div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
     fragment.appendChild(div);
   }
   boxes.appendChild(fragment);
 }
 
-function destroyBoxes() {
-  boxes.innerHTML = '';
-}
 
 function random() {
   return Math.floor(Math.random() * 256);
